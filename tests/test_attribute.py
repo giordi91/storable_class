d09bd241f321783effee2aa3fe230f2e05d890e6
Marco Giordano <mog@dneg.com>
2015-11-24 18:50:26 +0000

    finished tests on attribute

--- tests/test_attribute.py
+++ tests/test_attribute.py
@@ -1,5 +1,8 @@
 import unittest
+from functools import partial
+
 import attribute
+
 class Temp(object):
         def __init__(self, value):
             self.value = value
@@ -8,28 +11,100 @@ class TestGenericAttribute(unittest.TestCase):
 
     def test_creation(self):
 
-        test = attribute.GenericAttr()
-        test = 5
+        class TestClass(object):
 
-        test2 = attribute.GenericAttr()
-        test2 = "lol"
+            test = attribute.GenericAttr()
+            test2 = attribute.GenericAttr()
+            test3 = attribute.GenericAttr()
+
+        tmp = TestClass() 
+        tmp.test = 5
+        tmp.test2 = "foo"
+        tmp.test3 =  Temp("class instance")
         
-        test3 = attribute.GenericAttr()
-        test3 =  Temp("class instance")
-        
-        self.assertEqual(test , 5)
-        self.assertEqual(test2 , "lol")
-        self.assertEqual(test3.value , "class instance")
+        self.assertEqual(tmp.test , 5)
+        self.assertEqual(tmp.test2 , "foo")
+        self.assertEqual(tmp.test3.value , "class instance")
 
     def test_reassignment(self):
 
-        test = attribute.GenericAttr()
-        test = 5
+        class TestClass(object):
+            test = attribute.GenericAttr()
+            test2 = attribute.GenericAttr()
+        
+        tmp = TestClass() 
+        tmp.test = 5
+        tmp.test2 = "foo"
 
-        test2 = attribute.GenericAttr()
-        test2 = "lol"
+        tmp.test = "reassign"
+        tmp.test2= 10
 
-        test = "reassign"
-        test2= 10
-        self.assertEqual(test , "reassign")
-        self.assertEqual(test2 , 10)
+        self.assertEqual(tmp.test , "reassign")
+        self.assertEqual(tmp.test2 , 10)
+
+
+class TestTypedAttribute(unittest.TestCase):
+
+    def test_creation(self):
+        
+        class TestClass(object):
+            test = attribute.TypedAttr(["int"])
+            test2 = attribute.TypedAttr(["str"])
+            test3 = attribute.TypedAttr(["Temp"])
+
+        tmp = TestClass() 
+        tmp.test = 5
+        tmp.test2 = "foo"
+        tmp.test3 =  Temp("class instance")
+        
+        self.assertEqual(tmp.test , 5)
+        self.assertEqual(tmp.test2 , "foo")
+        self.assertEqual(tmp.test3.value , "class instance")
+
+    def test_default(self):
+        
+
+        #llooks like that descriptor is not triggered properly if first is not set
+        #so we need to set it in a class
+        class TestClass(object):
+
+            test = attribute.TypedAttr(["int"], 12)
+            test2 = attribute.TypedAttr(["str"], "fooBar")
+            test3 = attribute.TypedAttr(["Temp"])
+            test4 = attribute.TypedAttr(["float"], 12.0)
+        
+        tmp = TestClass()
+
+        self.assertEqual(tmp.test , 12)
+        self.assertEqual(tmp.test2 , "fooBar")
+        self.assertEqual(tmp.test3 , None)
+        self.assertEqual(tmp.test4 , 12.0)
+
+    
+    def test_multiple_values(self):
+
+        class TestClass(object):
+            test = attribute.TypedAttr(["int","float"])
+            test2 = attribute.TypedAttr(["str","bool"])
+
+        tmp = TestClass()
+        tmp.test = 5
+        tmp.test2 = "foo"
+        
+        self.assertEqual(tmp.test , 5)
+        self.assertEqual(tmp.test2 , "foo")
+
+        tmp.test = 12.23
+        tmp.test2 = False;
+        self.assertEqual(tmp.test , 12.23)
+        self.assertEqual(tmp.test2 , False)
+        
+        #here we get an hold of the descriptor, in order to avoid it to trigger
+        #the value setter or getter we access it trough the class definition dict
+        #and we get an hold to the function __set__
+        descript = TestClass.__dict__["test"].__set__
+        descript2 = TestClass.__dict__["test2"].__set__
+        
+        self.assertRaises(ValueError, descript, self,False)
+        self.assertRaises(ValueError, descript2, self, 12.2)
+
